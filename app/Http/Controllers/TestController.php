f6af718127427bfa490f3f8d0aee110bb1eb4623
robbindhilipjsparkout <robbindhilipjsparkout@sparkouttech.com>
2023-06-09 12:50:07 +0530

    machine task commit 2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -31,8 +31,6 @@ class TestController extends Controller
     public function create()
     {
        
-      
-        
         return view('machine_task.create_questions');
     
     }
@@ -43,56 +41,7 @@ class TestController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-//     public function store(Request $request)
-//     {
-    
-//         //dd($request->all());
-//         //dd($request->question);
 
-// foreach ($request->question as $q) {
-
-//  $result= Questions::create([
-        
-//         'questions' =>$q, 
-        
-//      ]);
-
-//  }
-
-
-//  foreach ($request->option1 as $opt1) {
-//     if (!empty($opt1)) {
-//         Options::create([
-//             'option1' => $opt1,
-//             'option2' => '', 
-//             'question_id' => $result->id,
-//         ]);
-//     }
-// }
-
-// foreach ($request->option2 as $opt2) {
-//     if (!empty($opt2)) {
-//         Options::create([
-//             'option1' => '', 
-//             'option2' => $opt2,
-//             'question_id' => $result->id,
-//         ]);
-//     }
-// }
-
-
-
-// // foreach($request->option as $opt){
-
-// //     Options::create([
-// //         'option1' => $opt,
-// //         'option2' => $opt, 
-// //         'question_id' => $result->id,
-// //     ]);
-// // }
-
-//  return redirect()->route('questionscreate')->with('message','Restaurant Created Successfully');
-// }
 
 public function store(Request $request){
 
@@ -170,29 +119,18 @@ public function score(Request $request)
             $correctAnswer = $question->QuestionToCorrectAnswer->first();  // Retrieve the correct answer instance
 
            // Check if the question ID exists as an index in the $answers array
-    if (isset($answers[$question->id])) {
-        if ($answers[$question->id] === $correctAnswer->correctanswer) {
-            $correctAnswers++;
-        }
-    } 
-    else {
-        
-    }
+           if (isset($correctAnswer) && isset($answers[$question->id]))    {
+            if ($answers[$question->id] === $correctAnswer->correctanswer) {
+                $correctAnswers++;
+            }
+            }    
+    
         $totalQuestions = count($questions);
         $score = ($correctAnswers / $totalQuestions) * 100;
 }
 
 
-
- // Send email
- $recipientEmail = 'robbindhilipj@gmail.com'; // Set the recipient email address here
- Mail::to($recipientEmail)->send(new TestScoreEmail($score));
-} else {
- // Handle the case when no answers are submitted
- $score = 0;
-}
-
-
+} 
 
 // Display the score
     return view('machine_task.score', compact('score', 'answers', 'questions'));
